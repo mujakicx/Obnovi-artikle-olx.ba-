7b053db43ac098fcf00c3b1db60fb1b1ffee48c9
mujakicx <mujakicdesign@gmail.com>
2021-04-14 18:17:37 +0200

    Create olx.js

---
+++ olx.js
@@ -0,0 +1,16 @@
+let htmlCollection = document.getElementsByClassName('artikal');
+let arr = [...htmlCollection];
+let articles = [];
+
+for (let i = 0; arr.length; i++) {
+    articles.push(arr[i].id.substr(4, 12));
+}
+
+const obnovi = () => {
+    for (let i = 0; i < articles.length; i++) {
+        obnoviartikal_mojpik(articles[i]);
+        console.log(`Artikal ${i} je uspješno obnovljen.`);
+    }
+}
+
+obnovi();
